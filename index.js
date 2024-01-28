27a4e42036e5c77c39b0dccdef017a6c9b96d17c
chase <yicha7@gmail.com>
2024-01-28 23:51:18 +0900

    sdf

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+const webpush = require('web-push')
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+let vapidKeys = webpush.generateVAPIDKeys();
+
+webpush.setVapidDetails(
+  'mailto:yicha7@gmail.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/noti', function (req, res) {
+  res.send(vapidKeys)
+})
+
+app.post('/sendNotification', function (req, res) {
+  setTimeout(function () {
+    webpush
+      .sendNotification(req.body.subscription)
+      .then(function () {
+        res.sendStatus(201);
+      })
+      .catch(function (error) {
+        res.sendStatus(500);
+        console.log(error);
+      });
+  }, 10000);
+  res.send('메세지 성공')
+})
+
+app.listen(3000)
