89e3503e16ccc590709a3338a17f40eb1d5b5823
chase <yicha7@gmail.com>
2024-02-05 07:10:46 +0900

    sdf

--- router/test.js
+++ router/test.js
@@ -2,19 +2,14 @@
 const testRouter = require('express').Router();
 const { MongoClient } = require('mongodb');
 
-let connectUrl = 'mongodb+srv://minkyu:6L3lCooFyja23Nmh@minkyu.rlol7cf.mongodb.net/?retryWrites=true&w=majority';
-let client = new MongoClient(connectUrl);
-let collection;
-
-const dbConnect = async ()=>{
-  
-}
-
 
 const crud = async (type, info)=>{
-    await client.connect(); 
-    const db = await client.db('bucket');
-    collection = await db.collection('bucket-list');
+  let connectUrl = 'mongodb+srv://minkyu:6L3lCooFyja23Nmh@minkyu.rlol7cf.mongodb.net/?retryWrites=true&w=majority';
+  let client = new MongoClient(connectUrl);
+
+  await client.connect(); 
+  const db = await client.db('bucket');
+  const collection = await db.collection('bucket-list');
 
   switch(type){
     case 'post':await collection.insertOne(info); break;
