0a652c8149b54dec82c718eddfbcdc79c5632190
chase <yicha7@gmail.com>
2024-02-05 05:05:07 +0900

    sdf

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config({ path: '.env'})
-const test = require('./router/test.js');
 const push = require('./router/push.js');
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/', test.testRouter);
+
 app.use('/push', push);
 
 
